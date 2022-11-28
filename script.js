367b8457b9591919b088b65b59b005eda260d621
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2022-11-28 12:05:05 -0500

    defined functions for prompt and confirms

--- script.js
+++ script.js
@@ -4,8 +4,39 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   // YOUR CODE GOES HERE
   // how many characters (8-128)
+  var charNum = function () {
+    prompt("How many characters?")
+  }
+  var upper = function () {
+    confirm("Would you like uppercase letters?")
+  }
+  var lower = function () {
+    confirm("Would you like lowercase letters?")
+  }
+  var numbers = function () {
+    confirm("Would you like numbers?")
+  }
+  var special = function () {
+    confirm("Would you like special characters?")
+  }
+
+  generateBtn.addEventListener("click", () => {
+    charNum()
+
+  })
   // uppercase?
+  if (charNum > 7 && charNum < 100) {
+    upper();
+  } else {
+    alert("Password must be between 8 and 100 characters, please try again")
+  }
+
+  lower();
+  numbers();
+  special();
+
   // lowercase?
+
   // numbers?
   // special characters?
 
