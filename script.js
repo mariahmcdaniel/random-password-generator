1018bd0e0df8f9a968c5cce22617953ba7e26bbf
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2022-11-28 17:55:46 -0500

    added conditionals for replacing chars- still needs editing

--- script.js
+++ script.js
@@ -44,12 +44,27 @@ function generatePassword() {
     pw.push(chooseFrom.charAt(Math.floor(Math.random() * chooseFromArr.length)))
   }
 
-  pwString = pw.join('');
+  var pwString = pw.join('');
 
-  numLower = (pwString.match(/[a-z]/g) || []).length;
-  numUpper = (pwString.match(/[A-Z]/g) || []).length;
-  numNums = (pwString.match(/[0-9]/g) || []).length;
-  numSpecial = (pwString.match(/[^A-Za-z0-9]/g) || []).length;
+  var numLower = (pwString.match(/[a-z]/g) || []).length;
+  var numUpper = (pwString.match(/[A-Z]/g) || []).length;
+  var numNums = (pwString.match(/[0-9]/g) || []).length;
+  var numSpecial = (pwString.match(/[^A-Za-z0-9]/g) || []).length;
+
+  var numLowerExp = numLower > 0;
+  var numUpperExp = numUpper > 0;
+  var numNumsExp = numNums > 0;
+  var numSpecialExp = numSpecial > 0;
+
+  if (numbers && !numNumsExp) {
+    special && numSpecialExp ? pwString.replace((pwString.search(/[^A-Za-z0-9]/g)), (Math.floor(Math.random() * 9))) : uppercase && numUpperExp ? pwString.replace((pwString.search(/[A-Z]/g)), (Math.floor(Math.random() * 9))) : pwString.replace((pwString.search(/[a-z]/g)), (Math.floor(Math.random() * 9)))
+  } else if (lowercase && !numLowerExp) {
+    special && numSpecialExp ? pwString.replace((pwString.search(/[^A-Za-z0-9]/g)), (lowercasePool.charAt(Math.floor(Math.random() * 26)))) : uppercase && !numUpperExp ? pwString.replace((pwString.search(/[A-Z]/g)), (lowercasePool.charAt(Math.floor(Math.random() * 26)))) : pwString.replace((pwString.search(/[0-9]/g)), (lowercasePool.charAt(Math.floor(Math.random() * 26))))
+  } else if (uppercase && !numUpperExp) {
+    special && numSpecialExp ? pwString.replace((pwString.search(/[^A-Za-z0-9]/g)), (uppercasePool.charAt(Math.floor(Math.random() * 26)))) : lowercase && numLowerExp ? pwString.replace((pwString.search(/[a-z]/g)), (uppercasePool.charAt(Math.floor(Math.random() * 26)))) : pwString.replace((pwString.search(/[0-9]/g)), (uppercasePool.charAt(Math.floor(Math.random() * 26))))
+  } else if (special && !numSpecialExp) {
+    uppercase && !numUpperExp ? pwString.replace((pwString.search(/[A-Z]/g)), (specialPool.charAt(Math.floor(Math.random() * 30)))) : lowercase && numLowerExp ? pwString.replace((pwString.search(/[a-z]/g)), (specialPool.charAt(Math.floor(Math.random() * 30)))) : pwString.replace((pwString.search(/[0-9]/g)), (specialPool.charAt(Math.floor(Math.random() * 30))))
+  }
 
   console.log(numLower);
   console.log(numUpper);
